a6033ca5d81681a4063086609ce4130f63f8b901
yudetamago <yudetamago.orz@gmail.com>
2016-09-27 03:32:54 +0900

    sinatra_appのさんぷる

---
+++ sinatra_app/domain/answer.rb
@@ -0,0 +1,8 @@
+module Domain
+  class Answer
+    def self.nearest_member_id(questions, answers)
+      # TODO: いい感じに近いメンバーを計算する
+      1
+    end
+  end
+end
---
+++ sinatra_app/domain/question.rb
@@ -0,0 +1,8 @@
+module Domain
+  class Question
+    def self.sorted(user_name, score, questions)
+      # TODO: いい感じに並べ替える
+      questions
+    end
+  end
+end
