b07f382a7fec9ff8092c6e12d316e5c9efc46635
morosawamikihito <morosawa.mikihito@gmail.com>
2016-09-27 14:10:01 +0900

    automatic create-write CSV-file for answer feature implements

--- sinatra_app/app.rb
+++ sinatra_app/app.rb
@@ -8,6 +8,7 @@ require_relative 'domain/question.rb'
 require_relative 'domain/answer.rb'
 require_relative 'infra/question.rb'
 
+=begin
 before do
   content_type :json
   Dir::chdir(settings.root)
@@ -19,6 +20,7 @@ configure do
   use Rack::CommonLogger, Logger.new("#{settings.root}/log/#{settings.environment}.log", 'daily')
   set :server, :puma
 end
+=end
 
 not_found do
   {error_code: 404, error_message: 'not found'}.to_json
@@ -32,12 +34,38 @@ error 400 do
   {error_code: 400, error_message: 'bad request'}.to_json
 end
 
+get '/home' do
+  @foo = 'foo'
+  erb :home
+end
+
+get '/index' do
+  content_type 'text/html; charset=utf8'
+  erb :index
+end
+
+get '/answer' do
+  question_id = params['question_id']
+  answer = params['answer']
+  csv_file_name = params['csv_file_name']
+  Domain::Answer.answer_write(question_id, answer, csv_file_name)
+end
+
 get '/start' do
+  # ユーザーの回答を記録するCSVファイルを作成
+  file_number = Dir.glob('./domain/answers/*.csv').count
+  csv_file_name = (file_number + 1).to_s + '.csv'
+  file_name = './domain/answers/' + csv_file_name
+  File.open(file_name,'w'){|file| file = nil}
+
+  content_type 'text/html; charset=utf8'
   user_name = params['user_name']
   score = params['score']
   questions = Infra::Question.all_questions
-  sorted_questions = Domain::Question.sorted(user_name, score, questions)
-  {questions: sorted_questions.map { |q| q.to_h.slice(:id, :statement)}}.to_json
+  @sorted_questions = Domain::Question.sorted(user_name, score, questions)
+  @csv_file_name = csv_file_name
+  erb :index
+  #{questions: sorted_questions.map { |q| q.to_h.slice(:id, :statement)}}.to_json
 end
 
 post '/end' do
--- sinatra_app/domain/answer.rb
+++ sinatra_app/domain/answer.rb
@@ -2,7 +2,12 @@ module Domain
   class Answer
     def self.nearest_member_id(questions, answers)
       # TODO: いい感じに近いメンバーを計算する
-      1
+      
+    end
+    def self.answer_write(question_id, answer, csv_file_name)
+      file = File.open('./domain/answers/' + csv_file_name, 'a')
+      file.puts question_id + ',' + answer
+      file.close
     end
   end
 end
